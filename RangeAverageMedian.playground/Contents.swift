f96ff36e9a785f63b6aafd730cc5eb0aac6a6894
Emily Corso <ekcorso@gmail.com>
2021-10-08 13:36:09 -0700

    Final version including formatting

--- RangeAverageMedian.playground/Contents.swift
+++ RangeAverageMedian.playground/Contents.swift
@@ -3,7 +3,7 @@
 
 import UIKit
 
-// This func should take a string in the format "hh|mm|ss, hh|mm|ss, hh|mm|ss, hh|mm|ss, hh|mm|ss" containing the race results of a an arbitrarily-sized team of runners. This func should calculate the range, mean, and median and return those values in the format "Range: hh|mm|ss Average: hh|mm|ss Median: hh|mm|ss"
+// This func should take a string in the format "hh|mm|ss,hh|mm|ss,hh|mm|ss,hh|mm|ss,hh|mm|ss" containing the race results of a an arbitrarily-sized team of runners. This func should calculate the range, mean, and median and return those values in the format "Range: hh|mm|ss Average: hh|mm|ss Median: hh|mm|ss"
 // If the imput string is empty, return ""
 // If the result in seconds is ab.xy it should be truncated as ab
 
@@ -11,12 +11,10 @@ import UIKit
 
 
 func stat(_ strg: String) -> String {
-    // Step 1: Break the string into individual scores by ","
-    // Step 2: Break the individual scores by "|" and convert to seconds
-    // Step 3: Calculate range, mean, and median (in seconds)
-    // Step 4: Re-format the data and return as a string
+    if strg.isEmpty { return strg }
+    
     var scoresInSeconds: [Int] {
-        let scores = strg.components(separatedBy: ", ")
+        let scores = strg.components(separatedBy: ",")
         var arrayOfScoresInSeconds = [Int]()
         for score in scores {
             let indScoreString = score.components(separatedBy: "|").map( {Int($0)} )
@@ -26,21 +24,27 @@ func stat(_ strg: String) -> String {
         return arrayOfScoresInSeconds
     }
     
-    let range: Int = scoresInSeconds.max()! - scoresInSeconds.min()!
-    let average: Int = scoresInSeconds.reduce(0, +)
-    var median: Int {
+    let rangeInSec: Int = scoresInSeconds.max()! - scoresInSeconds.min()!
+    let averageInSec: Int = scoresInSeconds.reduce(0, +) / scoresInSeconds.count
+    var medianInSec: Int {
         let length = scoresInSeconds.count
-        if length % 2 == 0 {
-            return ((scoresInSeconds[length / 2 - 1] + scoresInSeconds[length / 2]) / 2 )
-        } else {
-            return ((scoresInSeconds[length / 2]))
-        }
+        let sortedScores = scoresInSeconds.sorted()
+        return length % 2 == 0 ? ((sortedScores[length / 2 - 1] + sortedScores[length / 2]) / 2 ) : (sortedScores[length / 2])
     }
         
-    return "Range: \(range) Average: \(average) Median: \(median)"
+    func formatStat(_ stat: Int) -> String {
+        let hours = stat / 3600
+        let minutes = (stat - (hours * 3600)) / 60
+        let seconds = stat - (hours * 3600) - (minutes * 60)
+        let numberFormatter = NumberFormatter()
+        numberFormatter.minimumIntegerDigits = 2
+        return "\(numberFormatter.string(from: NSNumber(value:hours))!)|\(numberFormatter.string(from: NSNumber(value:minutes))!)|\(numberFormatter.string(from: NSNumber(value:seconds))!)"
+    }
+
+    return "Range: \(formatStat(rangeInSec)) Average: \(formatStat(averageInSec)) Median: \(formatStat(medianInSec))"
 }
 
-let sampleStats = "01|15|59, 1|47|6, 01|17|20, 1|32|34, 2|3|17"
+let sampleStats = "02|15|59,02|47|16,02|17|20,02|32|34,02|17|17,02|22|00,02|31|41"
 print(stat(sampleStats))
 
 
