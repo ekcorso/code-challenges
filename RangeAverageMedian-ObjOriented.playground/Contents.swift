65fa85ded241089497ac09e4630341d54b457faf
Emily Corso <ekcorso@gmail.com>
2021-10-09 16:08:39 -0700

    Refactor to move formatting to Stat

--- RangeAverageMedian-ObjOriented.playground/Contents.swift
+++ RangeAverageMedian-ObjOriented.playground/Contents.swift
@@ -10,18 +10,10 @@ import UIKit
 
 //**This version of the solution refactors the original to use classes instead of primitive types and otherwise be more object oriented. The Codewars problem doesn't ask for this, but I thought it was an interesting variation. I also changed the name of the function-- stat(_ strg:) was provided, but I don't think it's very expressive.
 
-struct Score {
-    let seconds: Int
-    //Is an individual score ever even necessary? If I'm only working in group stats, maybe it's not that important to break this out.
-    
-    // Each individual score could be initialized with the string (after it's broken into components)
-    
-}
-
-struct Stats {
-    var range: Int
-    var average: Int
-    var median: Int
+struct Stat {
+    var range: String
+    var average: String
+    var median: String
     
     init(scores: [Int]) {
         let rangeInSec: Int = scores.max()! - scores.min()!
@@ -32,10 +24,19 @@ struct Stats {
             return length % 2 == 0 ? ((sortedScores[length / 2 - 1] + sortedScores[length / 2]) / 2 ) : (sortedScores[length / 2])
         }
         
-        self.range = rangeInSec
-        self.average = averageInSec
-        self.median = medianInSec
+        self.range = format(rangeInSec)
+        self.average = format(averageInSec)
+        self.median = format(medianInSec)
     }
+}
+
+func format(_ stat: Int) -> String {
+    let hours = stat / 3600
+    let minutes = (stat - (hours * 3600)) / 60
+    let seconds = stat - (hours * 3600) - (minutes * 60)
+    let numberFormatter = NumberFormatter()
+    numberFormatter.minimumIntegerDigits = 2
+    return "\(numberFormatter.string(from: NSNumber(value:hours))!)|\(numberFormatter.string(from: NSNumber(value:minutes))!)|\(numberFormatter.string(from: NSNumber(value:seconds))!)"
 }
 
 func getTeamStats(_ statListStr: String) -> String {
@@ -51,22 +52,11 @@ func getTeamStats(_ statListStr: String) -> String {
         }
         return arrayOfScoresInSeconds
     }
-        
-    func formatStat(_ stat: Int) -> String {
-        let hours = stat / 3600
-        let minutes = (stat - (hours * 3600)) / 60
-        let seconds = stat - (hours * 3600) - (minutes * 60)
-        let numberFormatter = NumberFormatter()
-        numberFormatter.minimumIntegerDigits = 2
-        return "\(numberFormatter.string(from: NSNumber(value:hours))!)|\(numberFormatter.string(from: NSNumber(value:minutes))!)|\(numberFormatter.string(from: NSNumber(value:seconds))!)"
-    }
     
-    let teamStatsInSeconds = Stats(scores: scoresInSeconds)
-
-    return "Range: \(formatStat(teamStatsInSeconds.range)) Average: \(formatStat(teamStatsInSeconds.average)) Median: \(formatStat(teamStatsInSeconds.median))"
+    let teamStats = Stat(scores: scoresInSeconds)
+    
+    return "Range: \(teamStats.range) Average: \(teamStats.average) Median: \(teamStats.median)"
 }
 
 let sampleStats = "02|15|59,02|47|16,02|17|20,02|32|34,02|17|17,02|22|00,02|31|41"
 print(getTeamStats(sampleStats))
-
-
