fecb84d20c200781965b029614c01a8b9f5a8592
Emily Corso <ekcorso@gmail.com>
2021-10-17 18:08:27 -0700

    Intial commit for DisemvowelTrolls stub

---
+++ DisemvowelTrolls.playground/Contents.swift
@@ -0,0 +1,10 @@
+// "Disemvowel Trolls" challenge from Codewars
+
+//https://www.codewars.com/kata/52fba66badcd10859f00097e/train/swift
+
+import UIKit
+
+func disemvowel(_ s: String) -> String {
+
+    return "This is the return strong"
+}
