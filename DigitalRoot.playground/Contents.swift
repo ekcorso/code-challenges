6662f5bb8075d1a4d6013a8c849cafe118a208d8
Emily Corso <ekcorso@gmail.com>
2021-10-17 18:41:53 -0700

    Initial commit for DigitalRoot stub

---
+++ DigitalRoot.playground/Contents.swift
@@ -0,0 +1,9 @@
+// Sum of Digits/ Digital Root challenge from Codewars
+// https://www.codewars.com/kata/541c8630095125aba6000c00/train/swift
+
+import UIKit
+
+func digitalRoot(of number: Int) -> Int {
+    
+    return 42
+}
