0ddd72f2bd2550b94901388d34737ee9305fd033
Emily Corso <ekcorso@gmail.com>
2021-10-19 17:16:39 -0700

    Initial commit for SwapCaseUsingN

---
+++ SwapCaseUsingN.playground/Contents.swift
@@ -0,0 +1,13 @@
+// Swap Case Using N challenge from Codewars
+// https://www.codewars.com/kata/5f3afc40b24f090028233490/train/swift
+
+import UIKit
+
+func swap(_ s: String, n: Int) -> String {
+    let binaryInt = String(n, radix: 2)
+    print(binaryInt)
+        
+    return "Finished"
+}
+
+swap("Let's do this", n: 11)
